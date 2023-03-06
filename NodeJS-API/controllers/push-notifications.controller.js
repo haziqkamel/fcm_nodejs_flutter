45c36ee87b0d29bdb1ee46bc22300902d7a0437c
Haziq Kamel <haziqkamel@outlook.com>
2023-03-06 09:35:33 +0800

    feat.(FCM): send message to multiple devices

--- NodeJS-API/controllers/push-notifications.controller.js
+++ NodeJS-API/controllers/push-notifications.controller.js
@@ -11,6 +11,13 @@ function sendPushNotificationSpecificUserDevice(req, res, next) {
             notification: {
                 title: req.body.title,
                 body: req.body.content,
+                imageUrl: req.body.imageUrl,
+            },
+            android: {
+                notification: {
+                    icon: 'stock_ticker_update',
+                    color: '#7e55c3'
+                }
             },
             data: req.body.data,
             token: req.body.fcm_token
@@ -33,6 +40,50 @@ function sendPushNotificationSpecificUserDevice(req, res, next) {
     }
 }
 
+function sendMessageToMultipleDevices(req, res, next) {
+    try {
+        let message = {
+            notification: {
+                title: req.body.title,
+                body: req.body.content,
+            },
+            android: {
+                notification: {
+                    icon: 'stock_ticker_update',
+                    color: '#7e55c3'
+                }
+            },
+            data: req.body.data,
+            tokens: req.body.fcm_tokens
+        };
+
+        FCM.sendMulticast(message).then((response) => {
+            console.log(`${response.successCount} + messages were sent successfully`)
+            if (response.failureCount > 0) {
+                const failedTokens = [];
+                response.responses.forEach((resp, idx) => {
+                    if (!resp.success) {
+                        failedTokens.push(registrationTokens[idx]);
+                    }
+                });
+                console.log('List of tokens that caused failures: ' + failedTokens);
+            }
+            return res.status(200).send({
+                message: `Successfully sent message: ${response}`
+            })
+        }).catch((error) => {
+            console.log('Error sending message: ', error)
+            return res.status(500).send({
+                message: error
+            })
+        })
+    } catch (err) {
+        console.log(`An error has occured: ${err}`);
+        throw err;
+    }
+}
+
 module.exports = {
     sendPushNotificationSpecificUserDevice,
+    sendMessageToMultipleDevices
 }
--- NodeJS-API/routes/app.routes.js
+++ NodeJS-API/routes/app.routes.js
@@ -1,7 +1,15 @@
-const { sendPushNotificationSpecificUserDevice } = require('../controllers/push-notifications.controller')
+const { 
+    sendPushNotificationSpecificUserDevice, 
+    sendMessageToMultipleDevices 
+} = require('../controllers/push-notifications.controller')
 const express = require('express')
 const router = express.Router()
 
-router.post('/send-push-notification-specific-user-device', sendPushNotificationSpecificUserDevice)
+router.post('/send-push-notification-specific-user-device', 
+sendPushNotificationSpecificUserDevice
+);
+router.post('/send-message-to-multiple-devices', 
+sendMessageToMultipleDevices
+);
 
 module.exports = router
